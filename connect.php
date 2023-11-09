471f4895d0c78632835bfb64fbb9849345ede5db
Ricardo Andrés Lucis <54828855+HUDnero27@users.noreply.github.com>
2023-11-08 23:41:45 -0300

    actualización
    eliminación.

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost:3308", "root", "123456789", "sistema_votacion");
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Conectado";
-
-?>
